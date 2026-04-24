cdd934b6164f348e78d1670042b59ae4a7dfdfe3
ricardoe-g <ricardo.guilherme@aluno.ifsp.edu.br>
2026-04-24 14:35:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function alterarFundo() {
     document.body.classList.toggle("dark");
 }
-const meuFormulario = document.getElementByID('meuFormulario
+const meuFormulario = document.getElementByID('meuFormulario');
 formulario.addEventListener('submit', function(evento){
 	event.preventDefault();
 	const.novotitulo = document.getElementById('titulo').value;
