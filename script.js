a20a3843d29541c1d7a09e92124b23b87823b9e8
ricardoe-g <ricardo.guilherme@aluno.ifsp.edu.br>
2026-04-24 14:23:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function alterarFundo() {
+    document.body.classList.toggle("dark");
+}
+const meuFormulario = document.getElementByID('meuFormulario
+formulario.addEventListener('submit', function(evento){
+	event.preventDefault();
+	const.novotitulo = ducument.getElementByID('titulo').value;
+	const.novaImagem = ducument.getElementByID('link').value;
+	const.novoTexto = ducument.getElementByID('descricao').value;
+})
