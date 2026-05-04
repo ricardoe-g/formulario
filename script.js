169fd1f43ab3ca31ef3bf0c73da1762f8312edfb
ricardoe-g <ricardo.guilherme@aluno.ifsp.edu.br>
2026-05-03 21:29:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function alterarFundo() {
     document.body.classList.toggle("dark");
 }
-const meuFormulario = document.getElementByID('meuFormulario');
-formulario.addEventListener('submit', function(evento){
-	event.preventDefault();
-	const.novoTitulo = document.getElementById('titulo').value;
-	const.novaImagem = document.getElementById('link').value;
-	const.novoTexto = document.getElementById('descricao').value;
+const meuFormulario = document.getElementById('meuFormulario');
+const colecao = document.getElementById('colecao');
+
+meuFormulario.addEventListener('submit', function(evento){
+	evento.preventDefault();
+
+	
+	const novoTitulo = document.getElementById('titulo').value;
+	const novaImagem = document.getElementById('link').value;
+	const novoTexto = document.getElementById('descricao').value;
 
 	const novo = {
 		titulo: novoTitulo,
 		imagem: novaImagem,
 		texto: novoTexto
 	};
-})
+
+	
+	const card = `
+		<div class="card">
+			<img src="${novo.imagem}">
+			<h3>${novo.titulo}</h3>
+			<p>${novo.texto}</p>
+		</div>
+	`;
+
+	
+	colecao.innerHTML += card;
+
+	
+	meuFormulario.reset();
+});
