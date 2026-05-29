3561c50ad79709ee7934e94bd1ccfc048bd3fe8c
ricardoe-g <ricardo.guilherme@aluno.ifsp.edu.br>
2026-05-29 13:47:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,48 +5,36 @@ const meuFormulario = document.getElementById('meuFormulario');
 const colecao = document.getElementById('colecao');
 
 function renderizarCards(animais) {
-
-    const novoCard = `
+    return `
     <div class="card">
-
         <div class="letras">
-            <h3 id="titulo">${animais.titulo}</h3>
+            <h3>${animais.titulo}</h3>
             <p>${animais.texto}</p>
         </div>
 
         <div class="img">
-            <img src="${animais.imagem}" alt="${animais.descricaoIm}">
+            <img src="${animais.imagem}" alt="${animais.titulo}">
         </div>
-
     </div>
     `;
-
-    return novoCard;
 }
 
-meuFormulario.addEventListener('submit', function(evento){
-
+meuFormulario.addEventListener('submit', function (evento) {
     evento.preventDefault();
 
-    // CAPTURAR
     const novoTitulo = document.getElementById('titulo').value;
     const novaImagem = document.getElementById('link').value;
     const novoTexto = document.getElementById('descricao').value;
 
-    // OBJETO
     const novo = {
         titulo: novoTitulo,
         imagem: novaImagem,
-        texto: novoTexto,
-        descricaoIm: novoTitulo
+        texto: novoTexto
     };
 
-    // CHAMAR FUNÇÃO
-    renderizarCards(novo);
+    const card = renderizarCards(novo);
 
-    // EXIBIR
     colecao.innerHTML += card;
 
-    // LIMPAR
     meuFormulario.reset();
 });
