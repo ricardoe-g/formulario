35456eab69c8251790b561a590c9e7436ad84351
ricardoe-g <ricardo.guilherme@aluno.ifsp.edu.br>
2026-08-14 14:39:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 class Galeria {
   constructor() {
-    this.galeria = JSON.parse(localStorage.getItem('galeria')) || [];
-    this.colecao = document.getElementById('colecao');
-    this.formulario = document.getElementById('meuFormulario');
+    this.galeria = JSON.parse(localStorage.getItem("galeria")) || [];
+    this.colecao = document.getElementById("colecao");
+    this.formulario = document.getElementById("meuFormulario");
 
     this.mostrar();
 
-    this.formulario.addEventListener('submit', evento => {
+    this.formulario.addEventListener("submit", (evento) => {
       evento.preventDefault();
       this.adicionar();
     });
   }
 
   salvar() {
-    localStorage.setItem('galeria', JSON.stringify(this.galeria));
+    localStorage.setItem("galeria", JSON.stringify(this.galeria));
   }
 
   mostrar() {
-    this.colecao.innerHTML = this.galeria.map(item => `
-      <div class="card">
-        <div class="letras">
-          <h3>${item.titulo}</h3>
-          <p>${item.texto}</p>
+    this.colecao.innerHTML = "";
+
+    this.galeria.forEach(item => {
+      this.colecao.innerHTML += `
+        <div class="card">
+          <div class="letras">
+            <h3>${item.titulo}</h3>
+            <p>${item.texto}</p>
+          </div>
+
+          <div class="img">
+            <img src="${item.imagem}" alt="${item.titulo}">
+          </div>
         </div>
-        <div class="img">
-          <img src="${item.imagem}" alt="${item.titulo}">
-        </div>
-      </div>
-    `).join('');
+      `;
+    });
   }
 
   adicionar() {
-    this.galeria.push({
-      titulo: document.getElementById('titulo').value,
-      imagem: document.getElementById('link').value,
-      texto: document.getElementById('descricao').value
-    });
+    const novo = {
+      titulo: document.getElementById("titulo").value,
+      imagem: document.getElementById("link").value,
+      texto: document.getElementById("descricao").value
+    };
 
+    this.galeria.push(novo);
     this.salvar();
     this.mostrar();
     this.formulario.reset();
@@ -47,4 +53,4 @@ function alterarFundo() {
   document.body.classList.toggle("dark");
 }
 
-var galeria = new Galeria();
+const galeria = new Galeria();
